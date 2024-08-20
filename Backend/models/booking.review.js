4c7b29ea6648d6dc19dac76a0ac3a6323952e24e
jamisaiteja <jamisaiteja666@gmail.com>
2024-08-20 15:59:35 +0530

    boiler plate

--- Backend/models/booking.review.js
+++ Backend/models/booking.review.js
@@ -23,7 +23,11 @@ const bookingSchema = new mongoose.Schema({
 });
 
 // Index for property and user fields
-bookingSchema.index({ property: 1, user: 1 }); // without {unique:true},it wouldn’t prevent multiple bookings by the same user for the same property
+bookingSchema.index({ property: 1, user: 1 }, { unique: true });
+// Without { unique: true }:
+// The index would still optimize queries, but it wouldn’t prevent multiple bookings by the same user for the same property.
+// With { unique: true }:
+// The index not only optimizes queries but also enforces a business rule (e.g., a user can only book a property once).
 
 const Booking = mongoose.model('Booking', bookingSchema);
 module.exports = Booking;
